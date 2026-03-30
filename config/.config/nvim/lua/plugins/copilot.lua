b084ec829997b728cb7673cbb0cc227cc8209415
MertzAndreas <Andreasmertz11@gmail.com>
2026-03-30 17:34:17 +0200

    Removed unused plugins

--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -1,25 +1,21 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "BufReadPost",
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        keymap = {
-          next = "<M-n>",
-          prev = "<M-p>",
-          accept = "<M-y>",
-          dismiss = "<M-e>",
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        build = ":Copilot auth",
+        event = "BufReadPost",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = false,
+                keymap = {
+                    next = "<M-n>",
+                    prev = "<M-p>",
+                    accept = "<M-y>",
+                    dismiss = "<M-e>",
+                },
+            },
+            panel = { enabled = false },
         },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
     },
-  },
 }
